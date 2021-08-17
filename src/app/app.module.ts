b390585fcff5bb47956a2eda0c57aa1170fc3b4f
Pallefuury <68808774+Pallefuury@users.noreply.github.com>
2021-08-17 13:53:24 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { MainContentComponent } from "./main-content/main-content.component";
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, MainContentComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
